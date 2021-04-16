85dd946d226cdf1438275ffd8b3df6242926fea3
LecioVilela <lecio.vilela@yahoo.com>
2021-04-16 10:59:33 -0300

    Adicionar arquivos de projeto.

---
+++ ex01/CalcularValorTroco.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex01
+{
+    class CalcularValorTroco
+    {
+        static void Main(string[] args)
+        {
+            double valorProduto;
+            double valorPassadoPeloCliente;
+            
+
+            Console.WriteLine("Digite o valor do produto");
+            valorProduto = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o valor pago pelo cliente: ");
+            valorPassadoPeloCliente = double.Parse(Console.ReadLine());
+
+
+
+            if (valorPassadoPeloCliente < valorProduto)
+            {
+                Console.WriteLine("Valor insuficiente para pagamento!");
+                Console.ReadLine();
+            }
+            else if (valorPassadoPeloCliente == valorProduto)
+            {
+                Console.WriteLine("Não tem troco!");
+                Console.ReadLine();
+            }
+            else
+            {
+                double resultado = valorPassadoPeloCliente - valorProduto;
+
+                Console.WriteLine("O troco é: " + resultado);
+                Console.ReadLine();
+            }
+            
+
+
+
+        }
+    }
+}
---
+++ ex02/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cálculo de IMC");
+
+            Console.WriteLine("Digite seu peso (em Kg): ");
+            double peso = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digita sua altura (em metros): ");
+            double altura = double.Parse(Console.ReadLine());
+
+            double alturaQuadrado = altura * altura;
+            double indiceMassaCorporal = peso / alturaQuadrado;
+
+            Console.WriteLine("Seu IMC é: " + indiceMassaCorporal);
+
+            Console.ReadLine();
+        }
+    }
+}
